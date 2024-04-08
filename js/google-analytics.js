43b7e39887ee277f843e70feca5074de5b360a2c
tu211 <211tgh@gmail.com>
2024-04-09 00:13:24 +0800

    Update

---
+++ js/google-analytics.js
@@ -0,0 +1,13 @@
+(function () {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-F2F8ZLYW9V';
+    document.head.appendChild(script);
+  
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', 'G-F2F8ZLYW9V');
+  })();
